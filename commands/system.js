af113ed9f033677c03134f1336e8f0f7bcc8c846
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-09-06 12:22:34 +0100

    Update system.js

--- commands/system.js
+++ commands/system.js
@@ -341,7 +341,7 @@ cmd({
             desc: "is bot alive??"
         },
         async(Void, citel, text, isAdmins) => {
-            let alivemessage = Config.ALIVE_MESSAGE || `*A bot developed by ProfileCorruptedError.*`
+            let alivemessage = Config.ALIVE_MESSAGE || `*A bot developed by EXCEL.*`
             const alivtxt = `
 *Hello, ${citel.pushName},*
 _This is  ${tlang().title}._
