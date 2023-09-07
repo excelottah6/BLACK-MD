94b381471f6b536f9a35b8701d77e1d418a9bf0d
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-09-07 13:48:04 +0100

    BLACK-MD

--- commands/group.js
+++ commands/group.js
@@ -3,9 +3,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Blade-MD
+ * @project_name : BLACK-MD
  * @author : salmanytofficial <https://github.com/Bladeh4x>
- * @description : Bladeh4x,A Multi-functional whatsapp bot.
+ * @description : Black MD,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 
