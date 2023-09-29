6b694e6ebee169cab681aa792835eba73471d10c
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-09-29 23:56:05 +0100

    Add files via upload

--- commands/_help.js
+++ commands/_help.js
@@ -3,9 +3,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Black-MD
- * @author : excel <https://github.com/excelottah>
- * @description : BLACK,A Multi-functional whatsapp bot.
+ * @project_name : KING-MD
+ * @author : naveeddogar <https://github.com/naveeddogar>
+ * @description : King,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 
@@ -20,88 +20,89 @@ const Secktor = require('../lib/commands')
 
     //---------------------------------------------------------------------------
 Secktor.cmd({
-    pattern: "help",
-    alias: ["menu"],
-    desc: "Help list",
-    category: "general",
-    react: "📟",
-    filename: __filename
-}, async (Void, citel, text) => {
-    const { commands } = require('../lib');
-    if (text.split(" ")[0]) {
-        let arr = [];
-        const cmd = commands.find((cmd) => cmd.pattern === (text.split(" ")[0].toLowerCase()))
-        if (!cmd) return await citel.reply("*❌No Such commands.*");
-        else arr.push(`*🍁Command:* ${cmd.pattern}`);
-        if (cmd.category) arr.push(`*🧩Category:* ${cmd.category}`);
-        if (cmd.alias) arr.push(`*🧩Alias:* ${cmd.alias}`);
-        if (cmd.desc) arr.push(`*🧩Description:* ${cmd.desc}`);
-        if (cmd.use) arr.push(`*〽️Usage:*\n \`\`\`${prefix}${cmd.pattern} ${cmd.use}\`\`\``);
-        return await citel.reply(arr.join('\n'));
-    } else {
-        const cmds = {}
-        commands.map(async (command, index) => {
-            if (command.dontAddCommandList === false && command.pattern !== undefined) {
-                if (!cmds[command.category]) cmds[command.category] = []
-                cmds[command.category].push(command.pattern)
-            }
-        })
-        const time = moment(moment())
-            .format('HH:mm:ss')
-        moment.tz.setDefault('Asia/KOLKATA')
-            .locale('id')
-        const date = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
-        let total = await sck1.countDocuments()
-        let str = `╭────╮\n`;
-        str +=         '```' + `
-│ 🌟 ʙʟᴀᴄᴋ ᴍᴅ 🌟
-├────╯
-│ 🧑‍💻 Owner: excel
-│ 🤖 User: ${citel.pushName}
-│ ⚙️ Prefix:  ${prefix}
-│ 📊 Plugins:  ${commands.length}
-│ ⏱️ Uptime: ${runtime(process.uptime())} 
-│ 💾 Memory: ${formatp(os.totalmem() - os.freemem())}/${formatp(os.totalmem())}
-│ 📅 Date: ${date}
-│ 🎠 Theme:${tlang().title}
-│ 👨‍💻 Commands: ${Config.ownername}
-│ 🆚 Version: 1.0.0
-╰─────────◆
-` + '```';
-        for (const category in cmds) {
-            str += `╭────♛⃟ *${tiny(category)}* ♛⃟\n`;
-            if (text.toLowerCase() == category.toLowerCase()) {
-                str = `╭─────♛⃟ *${tiny(category)}* ♛⃟\n`;
-                for (const plugins of cmds[category]) {
-                    str += `┃│ ${fancytext(plugins, 1)}\n`;
-                }
-                str += `╰━━━━━━━━━━━━━──⊷\n`;
-                break;
+            pattern: "help",
+            alias: ["menu"],
+            desc: "Help list",
+            category: "general",
+            react: "📟",
+            filename: __filename
+        },
+        async(Void, citel, text) => {
+            const { commands } = require('../lib');
+            if (text.split(" ")[0]) {
+                let arr = [];
+                const cmd = commands.find((cmd) => cmd.pattern === (text.split(" ")[0].toLowerCase()))
+                if (!cmd) return await citel.reply("*❌No Such commands.*");
+                else arr.push(`*🍁Command:* ${cmd.pattern}`);
+                if (cmd.category) arr.push(`*🧩Category:* ${cmd.category}`);
+                if (cmd.alias) arr.push(`*🧩Alias:* ${cmd.alias}`);
+                if (cmd.desc) arr.push(`*🧩Description:* ${cmd.desc}`);
+                if (cmd.use) arr.push(`*〽️Usage:*\n \`\`\`${prefix}${cmd.pattern} ${cmd.use}\`\`\``);
+                return await citel.reply(arr.join('\n'));
             } else {
-                for (const plugins of cmds[category]) {
-                    str += `┃│ ${fancytext(plugins, 1)}\n`;
+                const cmds = {}
+                commands.map(async(command, index) => {
+                    if (command.dontAddCommandList === false && command.pattern !== undefined) {
+                        if (!cmds[command.category]) cmds[command.category] = []
+                        cmds[command.category].push(command.pattern)
+                    }
+                })
+                const time = moment(moment())
+                    .format('HH:mm:ss')
+                moment.tz.setDefault('Asia/KOLKATA')
+                    .locale('id')
+                const date = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
+                let total = await sck1.countDocuments()
+                let str = `╭────《 𝙆𝙄𝙉𝙂-𝙈𝘿 》───⊷❍\n`
+                str +=
+                    '```' + `┇⛥╭──────────────◆
+┇⛥┇ 𝙊𝙬𝙣𝙚𝙧: Naveed Dogar
+┃⛥┃ 𝙐𝙨𝙚𝙧: ${citel.pushName}
+┃⛥┃ 𝙋𝙧𝙚𝙛𝙞𝙭: ( ${prefix} )
+┃⛥┃ 𝙋𝙡𝙪𝙜𝙞𝙣𝙨: ${commands.length}
+┃⛥┃ 𝙐𝙥𝙩𝙞𝙢𝙚: ${runtime(process.uptime())}
+┃⛥┃ 𝙈𝙚𝙢: ${formatp(os.totalmem() - os.freemem())}
+┃⛥┃ 𝘿𝙖𝙩𝙚: ${date}
+┃⛥┃ 𝙑𝙚𝙧𝙨𝙞𝙤𝙣: 1.0.0
+┃⛥┃ ▎▍▌▌▉▏▎▌▉▐▏▌▎
+┃⛥┃ ▎▍▌▌▉▏▎▌▉▐▏▌▎
+┇⛥┇
+┇⛥╰━━━━━━━━━━━───◆
+╰━━━━━━━━━━━──⊷\n
+` + '```'
+                for (const category in cmds) 
+                {
+                   str += `╭────♛⃟㉿ *${tiny(category)}* ♛⃟㉿\n` ;
+                   if(text.toLowerCase() == category.toLowerCase()){ str = `╭─────♛⃟㉿ *${tiny(category)}* ♛⃟㉿\n` ;      
+                        for (const plugins of cmds[category]) { str += `┃⚝│ ${fancytext(plugins,1)}\n` ; }
+                        str += `╰━━━━━━━━━━━━━──⊷\n`  ;
+                        break ;
+                   }
+                   else { for (const plugins of cmds[category]) { str += `┃⚝│ ${fancytext(plugins,1)}\n` ; }
+                         str += `╰━━━━━━━━━━━━━━──⊷\n`  ; 
+                   }
+  
                 }
-                str += `╰━━━━━━━━━━━━━━──⊷\n`;
+                str+= `*💠Type:* _${prefix}help cmd_ name to know more about specific command.\n*Eg:* _${prefix}help attp_\n*_🐉Mᴀᴅᴇ Bʏ Nᴀᴠᴇᴇᴅ Dᴏɢᴀʀ_* `
+                let buttonMessaged = {
+                    image: { url: await botpic() },
+                    caption: str
+                };
+                return await Void.sendMessage(citel.chat, buttonMessaged);
             }
         }
-        str += `*💠Type:* _${prefix}help cmd_ name to know more about specific command.\n*Eg:* _${prefix}help attp_\n*_🐉Mᴀᴅᴇ Bʏ EXCEL_* `;
-        let buttonMessaged = {
-            image: { url: await botpic() },
-            caption: str
-        };
-        return await Void.sendMessage(citel.chat, buttonMessaged);
-    }
-});
-
+    )
+    //---------------------------------------------------------------------------
 Secktor.cmd({
-    pattern: "list",
-    desc: "list menu",
-    category: "general"
-}, async (Void, citel) => {
-    const { commands } = require('../lib');
-    let str = `
+            pattern: "list",
+            desc: "list menu",
+            category: "general"
+        },
+        async(Void, citel) => {
+            const { commands } = require('../lib');
+            let str = `
 ╭━━〘 ` + fancytext(Config.ownername.split(' ')[0], 58) + ` 〙━━──⊷`
-    str += `
+            str += `
 ┃ ⛥╭──────────────      
 ┃ ⛥│ User: ${citel.pushName}
 ┃ ⛥│ Theme: ${tlang().title}
@@ -112,26 +113,29 @@ Secktor.cmd({
 ┃ ⛥│ Mem: ${formatp(os.totalmem() - os.freemem())}/${formatp(os.totalmem())}
 ┃ ⛥│  
 ┃ ⛥╰───────────
-╰━━━━━━━━━━━──⊷\n`;
-    for (let i = 0; i < commands.length; i++) {
-        if (commands[i].pattern == undefined) continue;
-        str += `╭ ${i + 1} *${fancytext(commands[i].pattern, 1)}*\n`;
-        if (commands[i].desc = undefined) commands[i].desc = "";
-        str += `╰➛ ${fancytext(commands[i].desc, 1)}\n`;
-    }
-    return await Void.sendMessage(citel.chat, { image: { url: THUMB_IMAGE }, caption: str });
-});
-
+╰━━━━━━━━━━━──⊷\n`
+for (let i = 0; i < commands.length; i++) 
+{
+     if(commands[i].pattern==undefined) continue
+     str +=       `╭ ${i+1} *${fancytext(commands[i].pattern,1)}*\n` 
+     if(commands[i].desc=undefined) commands[i].desc=""
+     str += `╰➛ ${fancytext(commands[i].desc,1)}\n`
+}
+            return await Void.sendMessage(citel.chat, { image: { url: THUMB_IMAGE }, caption: str })
+        }
+    )
+    //---------------------------------------------------------------------------
 Secktor.cmd({
-    pattern: "owner",
-    desc: "To find owner number",
-    category: "general",
-    react: "💜",
-    filename: __filename
-}, async (Void, citel) => {
-    const Config = require('../config')
-    const vcard = 'BEGIN:VCARD\n' +
-        'VERSION:3.0\n' +
+        pattern: "owner",
+        desc: "To find owner number",
+        category: "general",
+        react: "💜",
+        filename: __filename
+    },
+    async(Void, citel) => {
+        const Config = require('../config')
+        const vcard = 'BEGIN:VCARD\n' +
+            'VERSION:3.0\n' +
             'FN:' + Config.ownername + '\n' +
             'ORG:;\n' +
             'TEL;type=CELL;type=VOICE;waid=' + owner[0] + ':+' + owner[0] + '\n' +
@@ -169,7 +173,7 @@ async(Void, citel, text) => {
  const { commands } = require('../lib');
  let arr = [];
         const cmd = commands.find((cmd) => cmd.pattern === (text.split(" ")[0].toLowerCase()))
-        if (!cmd) return await citel.reply("*🥲No Such commands.*");
+        if (!cmd) return await citel.reply("*❌No Such commands.*");
         else arr.push(`*🍁Command:* ${cmd.pattern}`);
         if (cmd.category) arr.push(`*🧩Type:* ${cmd.category}`);
         if(cmd.filename) arr.push(`✨FileName: ${cmd.filename}`)
